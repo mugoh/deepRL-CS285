795b5ff8196be5a9bdac5db80e958a45d18d17e9
mugoh <mugoh.ks@gmail.com>
2020-05-25 11:54:00 +0300

    generate plots for q2 seeds

--- hw3/generate_plot.py
+++ hw3/generate_plot.py
@@ -10,6 +10,8 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
 
 def plot():
     """
@@ -18,10 +20,11 @@ def plot():
     qst = handle_input()
 
     os.chdir('cs285/.figures')
+    qst = '_'.join([x for x in qst])
     globals()[qst]()
 
 
-def q1():
+def q_1():
     """
         Basic DQN
     """
@@ -33,22 +36,61 @@ def q1():
     mean_path = paths[paths.index('q1_mean_return.csv')]
     best_path = paths[paths.index('q1_best_return.csv')]
 
-    draw_plot(axs[0], mean_path, mean_path.split('.')[0])
-    draw_plot(axs[1], best_path, best_path.split('.')[0])
+    draw_plot(axs[0], mean_path.split('.')[0], path=mean_path)
+    draw_plot(axs[1], best_path.split('.')[0], path=best_path)
     fig.tight_layout()
 
     plt.show()
 
 
-def draw_plot(axis, path, title):
+def q_2():
+    """
+        Seeded Double DQN
+    """
+    dqn_paths = glob.glob('*q2_dqn*')
+    ddqn_paths = glob.glob('*q2_ddqn*')
+
+    dqn_figs = average_seed(dqn_paths)
+    ddqn_figs = average_seed(ddqn_paths)
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Q2', x=0.2, y=0.97, size=11)
+
+    draw_plot(axs[0], title='DQN', figures=dqn_figs, change_ticks=1)
+    draw_plot(axs[1], title='DDQN', figures=ddqn_figs, change_ticks=1)
+    fig.tight_layout()
+
+    plt.show()
+
+
+def average_seed(paths):
+    """
+        Gets the mean of seeded runs
+    """
+    fig = np.asanyarray([np.loadtxt(path,
+                                    delimiter=',',
+                                    skiprows=1,
+                                    max_rows=49) for path in paths])
+    return fig.mean(axis=0)
+
+
+def draw_plot(axis, title, path=None, figures=None, change_ticks=False):
     """
         Draws plots using loaded csv data
     """
     title = title.replace('_', ' ')
-    figures = np.loadtxt(path, delimiter=',', skiprows=1)
+    figures = np.loadtxt(path, delimiter=',',
+                         skiprows=1) if not np.any(figures) else figures
     axis.plot(figures[:, 1], figures[:, 2], label=title)
     axis.set_title(title, fontdict={'size': 10})
     axis.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+
+    if change_ticks:
+        axis.yaxis.set_major_locator(MultipleLocator(50))
+        axis.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+        axis.yaxis.set_minor_locator(MultipleLocator(10))
+
+    axis.grid()
 
 
 def handle_input():
