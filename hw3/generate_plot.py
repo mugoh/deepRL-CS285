42e1fb02946f6f8dc623c198cbd772053e75cb5a
mugoh <mugoh.ks@gmail.com>
2020-05-25 10:27:02 +0300

    add plot for q1

--- hw3/generate_plot.py
+++ hw3/generate_plot.py
@@ -1,11 +1,57 @@
+"""
+    Creates plots for the questions with
+    data generated during training
+"""
 from sys import argv
+
+import os
+import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def plot():
-    qst = handle_input(argv)
+    """
+        Script Input
+    """
+    qst = handle_input()
+
+    os.chdir('cs285/.figures')
+    globals()[qst]()
 
 
-def handle_input(argv):
+def q1():
+    """
+        Basic DQN
+    """
+    paths = glob.glob('*q1*')
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Q1 PongNoFrameskip-v4', x=0.2, y=0.97, size=11)
+
+    mean_path = paths[paths.index('q1_mean_return.csv')]
+    best_path = paths[paths.index('q1_best_return.csv')]
+
+    draw_plot(axs[0], mean_path, mean_path.split('.')[0])
+    draw_plot(axs[1], best_path, best_path.split('.')[0])
+    fig.tight_layout()
+
+    plt.show()
+
+
+def draw_plot(axis, path, title):
+    """
+        Draws plots using loaded csv data
+    """
+    title = title.replace('_', ' ')
+    figures = np.loadtxt(path, delimiter=',', skiprows=1)
+    axis.plot(figures[:, 1], figures[:, 2], label=title)
+    axis.set_title(title, fontdict={'size': 10})
+    axis.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+
+
+def handle_input():
     """
         Handles input error
     """
